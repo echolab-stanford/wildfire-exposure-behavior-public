94d26b721f0465341ad5bdf7076b36d89b806402
jb-li <79426034+jb-li@users.noreply.github.com>
2022-05-26 15:38:17 -0400

    Incorporate table S10

---
+++ scripts/09_08_tableS10.R
@@ -0,0 +1,43 @@
+
+
+dat <- read_rds(file.path(path_dropbox, "epa_smoke_clean.rds"))
+
+
+dat$unitFE <- dat$id
+dat$unitmonthFE <- as.numeric(as.factor(paste(dat$id, dat$month, sep = "-")))
+dat$timeFE <- as.numeric(dat$date - as.Date("2005-12-31"))
+
+
+#smoke day
+
+mod1 <- (feols(pm25 ~ smoke_day | unitmonthFE + timeFE, data = dat[!is.na(dat$low_count),], cluster = "county"))
+
+#plume counts by density
+
+dat$low_dummy <- as.numeric(dat$low_count >0)
+dat$low_dummy[is.na(dat$low_count)]<-NA
+
+dat$med_dummy <- as.numeric(dat$med_count >0)
+dat$med_dummy[is.na(dat$med_count)]<-NA
+
+dat$high_dummy <- as.numeric(dat$high_count >0)
+dat$high_dummy[is.na(dat$high_count)]<-NA
+
+
+
+mod2 <- (feols(pm25 ~ low_dummy + med_dummy + high_dummy |   unitmonthFE + timeFE, data = dat[!is.na(dat$low_count),], cluster = "county"))
+
+
+
+etable(mod1, mod2, tex = T)
+
+
+xx = dat[!is.na(dat$low_count),]
+
+xx$pm25_r <- summary(feols(pm25 ~ unitmonthFE + timeFE, data = xx))$residuals
+xx$smoke_day_r <- summary(feols(smoke_day ~ unitmonthFE + timeFE, data = xx))$residuals
+xx$low_dummy_r <- summary(feols(low_dummy ~ unitmonthFE + timeFE, data = xx))$residuals
+xx$med_dummy_r <- summary(feols(med_dummy ~ unitmonthFE + timeFE, data = xx))$residuals
+xx$high_dummy_r <- summary(feols(high_dummy ~ unitmonthFE + timeFE, data = xx))$residuals
+
+summary(feols(pm25_r ~ smoke_day_r + -1, data= xx))
